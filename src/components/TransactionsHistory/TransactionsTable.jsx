0b65112620e27ec3f7e78c6e79df5b859214489c
unknown <andrijgrineveckij@gmail.com>
2023-01-13 16:07:06 +0200

    transactions component propTypes improvement

--- src/components/TransactionsHistory/TransactionsTable.jsx
+++ src/components/TransactionsHistory/TransactionsTable.jsx
@@ -27,9 +27,9 @@ TransactionsTable.propTypes = {
   transactions: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
-      type: PropTypes.string,
-      amount: PropTypes.string,
-      currency: PropTypes.string,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
     })
   ).isRequired,
 };
